ad99db5ee0bb08d1c16cbda0741fc35394d6cc56
ST10474705 <ST10474705@vcconnect.edu.za>
2025-06-19 11:00:28 +0200

    2nd page UI design

--- app/src/main/java/za/co/varsitycollege/imad_practicum_st10474705/DetailedViewScreen.kt
+++ app/src/main/java/za/co/varsitycollege/imad_practicum_st10474705/DetailedViewScreen.kt
@@ -2,11 +2,9 @@ package za.co.varsitycollege.imad_practicum_st10474705
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class DetailedViewScreen : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
@@ -14,14 +12,5 @@ class DetailedViewScreen : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_detailed_view_screen)
-
-        val bundle: Bundle? = intent.extras
-        val tableString: String? = bundle?.getString("songTitle"+"artistName"+"comments"+"rating")
-
-        val rating = tableString!!.toInt()
-
-        val displayTextView = findViewById<TextView>(R.id.displaytextView)
-
-        displayTextView.text = "$rating"
     }
 }
--- app/src/main/java/za/co/varsitycollege/imad_practicum_st10474705/MainActivity.kt
+++ app/src/main/java/za/co/varsitycollege/imad_practicum_st10474705/MainActivity.kt
@@ -21,10 +21,7 @@ class MainActivity : AppCompatActivity() {
         val addButton = findViewById<Button>(R.id.addButton)
         val exitAppButton = findViewById<Button>(R.id.exitAppButton)
         val songDetailsButton = findViewById<Button>(R.id.songDetailsButton)
-        val songTitleText = findViewById<EditText>(R.id.songTitleTextText)
-        val artistNameText = findViewById<EditText>(R.id.artistNameTextText)
-        val commentsText = findViewById<EditText>(R.id.commentsTextText)
-        val ratingTextNumber = findViewById<EditText>(R.id.ratingTextNumber)
+        var ratingTextNumber = findViewById<EditText>(R.id.ratingTextNumber)
 
         addButton.setOnClickListener {
             Toast.makeText(this@MainActivity,
@@ -33,16 +30,14 @@ class MainActivity : AppCompatActivity() {
         }
 
         songDetailsButton.setOnClickListener {
-            Toast.makeText(this@MainActivity,
-                "Button Clicked", Toast.LENGTH_LONG).show()
 
-            val intent = Intent(this, DetailedViewScreen::class.java)
+            val intent=Intent(this, DetailedViewScreen::class.java)
+
+            intent.putExtra("Rating",
+                            ratingTextNumber.text.toString())
+
             startActivity(intent)
 
-            intent.putExtra("SongTitle",songTitleText.text.toString())
-            intent.putExtra("ArtistName",artistNameText.text.toString())
-            intent.putExtra("Comments",commentsText.text.toString())
-            intent.putExtra("Rating",ratingTextNumber.text.toString())
         }
 
         exitAppButton.setOnClickListener {
